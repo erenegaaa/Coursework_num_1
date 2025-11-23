22cc6f23878d6d267ce80bc36ab51d663711d719
konstantin <erenegaa136@gmail.com>
2025-11-23 15:09:29 +0300

    Исправление ошибок

--- src/utils.py
+++ src/utils.py
@@ -6,18 +6,18 @@
 from typing import List, Dict, Any
 import json
 import logging
-from datetime import datetime
+import numpy as np
 import pandas as pd
 import re
-
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-def read_transaction_from_excel(path: str, sheet_name: str=0) -> pd.DataFrame:
+def read_transactions_from_excel(path: str, sheet_name: str=0) -> pd.DataFrame:
     """Считывание транзакции из excel файла"""
-    logger.info("Чтение транзакции из %s (sheet=%s)")
+    logger.info("Чтение транзакции из %s (sheet=%s)", path, sheet_name)
     df = pd.read_excel(path, sheet_name=sheet_name)
     df.columns = [c.strip() if isinstance(c, str) else c for c in df.columns] # один регистр
     return df
@@ -29,10 +29,14 @@ def df_to_json_serializable(df: pd.DataFrame) -> List[Dict[str, Any]]:
     for _, row in df.iterrows():
         obj = {}
         for col, value in row.items():
-            if isinstance(value, (pd.Timestamp, pd.DataFrame)):
+            if isinstance(value, (pd.Timestamp, datetime)):
                 obj[col] = value.strftime("%Y-%m-%d %H:%M:%S")
             elif pd.isna(value):
                 obj[col] = None
+            elif isinstance(value, (np.integer, np.int64)):
+                obj[col] = int(value)
+            elif isinstance(value, (np.floating, np.float64)):
+                obj[col] = float(value)
             else:
                 obj[col] = value
         result.append(obj)
@@ -45,7 +49,7 @@ def dumps_json(obj: Any, ensure_ascii: bool = False) -> str:
     return json.dumps(obj, ensure_ascii=ensure_ascii, indent=2)
 
 
-PHONE_RE = re.compile(r'(\+?\d{1,3}[\s\-]?)?(\(?\d{3,4}\)?[\s\-]?)?[\d\-\s]{5,}')
+PHONE_RE = re.compile(r'(\+?7|8)[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}')
 
 
 def extract_phone(text: str) -> str | None:
