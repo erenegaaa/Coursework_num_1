c7a5a976141889f614e51fbd9efff0846219a081
konstantin <erenegaa136@gmail.com>
2025-11-23 14:13:48 +0300

    Функция чтения excel

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,24 @@
 что в дальнейшем будет использоваться для Json файла транзакций.
 """
 
+from typing import List, Dict, Any
+import json
+import logging
+from datetime import datetime
+import pandas as pd
+import re
 
-def read_transaction_from_excel():
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+def read_transaction_from_excel(path: str, sheet_name: str=0) -> pd.DataFrame:
     """Считывание транзакции из excel файла"""
-    pass
+    logger.info("Чтение транзакции из %s (sheet=%s)")
+    df = pd.read_excel(path, sheet_name=sheet_name)
+    df.columns = [c.strip() if isinstance(c, str) else c for c in df.columns] # один регистр
+    return df
 
 
 def df_to_json_serializable():
