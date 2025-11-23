a04ae063e2889682dc8eccbbe08bb332abe3542b
konstantin <erenegaa136@gmail.com>
2025-11-23 13:39:20 +0300

    Подготовка функций

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+"""
+Модуль для чтения Excel,
+что в дальнейшем будет использоваться для Json файла транзакций.
+"""
+
+
+def read_transaction_from_excel():
+    """Считывание транзакции из excel файла"""
+    pass
+
+
+def df_to_json_serializable():
+    """Преобразование Dataframe в список словарей для json"""
+    pass
+
+
+def dumps_json():
+    """обертка json файла"""
+    pass
+
+
+def extract_phone():
+    """Поиск номера телефона в тексте транзакции"""
+    pass
